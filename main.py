7fa7514cb9217c4687edacc25b7b7841d075a4e9
Parth11791 <parthpandey300@gmail.com>
2025-07-31 23:03:10 +0000

    adding spaceship and its movements left and right

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from circleshape import *
+from player import *
 from constants import *
 pygame.init()
 def main():
@@ -6,12 +8,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    andy = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
           for event in pygame.event.get():
               if event.type == pygame.QUIT:
                  return
           pygame.Surface.fill(screen, (0, 0, 0), rect=None)
+          andy.update(dt)
+          andy.draw(screen)
           pygame.display.flip()
+          dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
